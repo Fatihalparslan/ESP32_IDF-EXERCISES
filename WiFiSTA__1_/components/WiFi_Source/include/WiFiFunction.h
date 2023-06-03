46f4a131d7cec0e4fc1d8eb67985967f5f288c7d
Fatihalparslan <fatih22alparslan@gmail.com>
2023-06-04 00:12:56 +0300

    WiFi Example 1 has add

--- WiFiSTA__1_/components/WiFi_Source/include/WiFiFunction.h
+++ WiFiSTA__1_/components/WiFi_Source/include/WiFiFunction.h
@@ -9,8 +9,8 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-#define EXAMPLE_ESP_WIFI_SSID      "TP-Link_3CDE"
-#define EXAMPLE_ESP_WIFI_PASS      "23667464"
+#define EXAMPLE_ESP_WIFI_SSID      "xxx-xxxx"
+#define EXAMPLE_ESP_WIFI_PASS      "xxxxxxxx"
 #define EXAMPLE_ESP_MAXIMUM_RETRY  2
 
 void WiFi_Start_Sta(void);
